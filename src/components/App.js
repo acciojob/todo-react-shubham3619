341727eaaa226bc29f135e8e4d3f3837c7ad613a
shubham3619 <shubhamsharma3619@gmail.com>
2024-01-18 13:00:53 +0530

    submitting code-Thu Jan 18 2024 13:00:52 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,46 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [toDo, setTodo] = useState("")
+  const [task, setTask] = useState("")
+  
+  function handleInput(e) {
+    setTodo(e.target.value)
+  }
+
+  function addBtn() {
+    setTask(toDo)
+    setTodo("")
+  }
+
+  function deleteBtn() {
+    setTask("")
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h3>To-Do List</h3>
+      <input
+        value={toDo}
+        onChange={handleInput}
+      />
+      <button type="submit" onClick={addBtn}>Add Todo</button>
+
+
+      {
+        task && (
+          <div>
+            <p>{task}</p>
+            <button onClick={deleteBtn}>Delete</button>
+          </div>
+
+        )
+      }
+
+      
+
     </div>
   )
 }
